982577b2f5a3fa5610a9f13cd6a607457590e95c
Filipe Ximenes <filipeximenes@gmail.com>
2016-08-26 17:32:04 -0300

    a΅

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -13,11 +13,6 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-% disp(X)
-% disp(theta' * X(1,:)')
-
-% disp(X * theta)
-
 J = (1 / (2 * m)) * sum(((X * theta) - y).^2);
 
 % =========================================================================
